403eaef3a3216aa3ea7d75e8d5b2ce6e3d4d4210
rmartm14 <rmartm14@estudiantes.unileon.es>
2022-02-17 11:36:31 +0100

    1- Create Endpoint for a politician creation

--- back/src/app.py
+++ back/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from politician import Politician
 from csvReader import read_csv, convert_csv_to_politians
 
 app = Flask(__name__)
@@ -6,6 +7,25 @@ app = Flask(__name__)
 def hello_world():
     convert_csv_to_politians(read_csv())
     return ""
+
+@app.route("/politician/", methods=['POST'])
+def create_politician():
+    """
+    Endpoint to create a politician by the body request
+
+    Args:
+    Returns:
+        politician(dict): Dictionary with the politician created (Provisional, then it may send OK - 200)
+    """
+    request_data = request.get_json()
+
+    #Check if there is request_data
+    if request_data:
+        pol = Politician(request_data)
+        #TODO:Insert this data on mongo!
+        return pol.__dict__
+    return 'request_data is empty'
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- back/src/politician.py
+++ back/src/politician.py
@@ -19,5 +19,21 @@ class Politician():
       self.retribucion_mensual = retribucion_mensual
       self.retribucion_anual = retribucion_anual
       self.observaciones = observaciones
-
     
+    def __init__(self, request_data):
+        self.titular = request_data['titular']
+        self.partido = request_data['partido']
+        self.partido_filtro = request_data['partido_filtro']
+        self.genero = request_data['genero']
+        self.cargo_filtro = request_data['cargo_filtro']
+        self.cargo = request_data['cargo']
+        self.institucion = request_data['institucion']
+        self.ccaa = request_data['ccaa']
+        self.sueldobase_sueldo = request_data['sueldobase_sueldo']
+        self.complementos_sueldo = request_data['complementos_sueldo']
+        self.paga_sueldo = request_data['paga_sueldo']
+        self.dieta_sueldo = request_data['dieta_sueldo']
+        self.trienios_sueldo = request_data['trienios_sueldo']
+        self.retribucion_mensual = request_data['retribucion_mensual']
+        self.retribucion_anual = request_data['retribucion_anual']
+        self.observaciones = request_data['observaciones']
